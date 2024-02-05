5240a2c32d22ee8924802def0bf1312bc725c9cd
itsSoheil <soheil.samadi79@gmail.com>
2024-02-05 11:40:02 +0330

    Creat Course Wrapper and a Course

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,38 @@ function App() {
   return (
     <div>
       <h1 className="">My Courses (3)</h1>
+      <div className="course-list">
+        <div className="course-item">
+          <div className="course-item__img">
+            <img src="../public/images/img1.jpg" alt="" />
+          </div>
+          <div className="course-item__detail">
+            <div className="course-item__body">
+              <div>
+                <p className="title">React Js Course</p>
+                <p className="desc">The Ultimate React and Redux Course</p>
+              </div>
+              <div className="rate">4</div>
+            </div>
+            <div className="course-item__footer">
+              <div className="tags">
+                <span className="badge badge--secondary">React JS</span>
+                <span className="badge badge--secondary">Frountend</span>
+              </div>
+              <div className="caption">
+                <div className="date">
+                  {new Date().toLocaleDateString("en-US", {
+                    month: "short",
+                    year: "numeric",
+                    day: "numeric",
+                  })}
+                </div>
+                <span className="badge badge--danger">Completed</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
